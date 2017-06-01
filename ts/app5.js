092b880cec2f290a629eeb586ea121b6b56b82d8
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2017-06-01 10:54:36 +0530

    systemjs added and import export es6 used

---
+++ ts/app5.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Circle = require("../math/circle");
+var rectangle_1 = require("../math/rectangle");
+console.log(Circle.PI);
+console.log(Circle.calculateCircumference(10));
+console.log(rectangle_1.calculateRectangle(10, 20));
---
+++ ts/app5.ts
@@ -0,0 +1,6 @@
+import * as Circle from "../math/circle";
+import { calculateRectangle } from "../math/rectangle";
+
+console.log(Circle.PI);
+console.log(Circle.calculateCircumference(10));
+console.log(calculateRectangle(10, 20));
