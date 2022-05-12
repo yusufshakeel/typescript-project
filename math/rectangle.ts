f3ea77d8bcdcc9b459f5b5c7d6421d2d150c8020
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2022-05-12 20:52:18 +0530

    update

--- math/rectangle.ts
+++
@@ -1,3 +0,0 @@
-export function calculateRectangle(width: number, length: number) {
-	return width * length;
-}
--- ts/app5.ts
+++
@@ -1,6 +0,0 @@
-import * as Circle from "../math/circle";
-import { calculateRectangle } from "../math/rectangle";
-
-console.log(Circle.PI);
-console.log(Circle.calculateCircumference(10));
-console.log(calculateRectangle(10, 20));
